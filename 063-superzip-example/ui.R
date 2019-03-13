8463a0dd38a1e3e6c7c01f459edcb7d364d3a325
Fengjunjie Pan <f.pan@campus.tu-berlin.de>
2019-03-13 20:04:01 +0100

    put two tab in pne page

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -10,9 +10,9 @@ vars <- c(
 )
 
 
-navbarPage("Superzip", id="nav",
-
-  tabPanel("Interactive map",
+#navbarPage("Superzip", id="nav",
+tabsetPanel(
+  tabPanel(
     div(class="outer",
 
       tags$head(
@@ -22,7 +22,7 @@ navbarPage("Superzip", id="nav",
       ),
 
       # If not using custom CSS, set height of leafletOutput to a number instead of percent
-      leafletOutput("map", width="100%", height="100%"),
+      leafletOutput("map", width="50%", height="50%"),
 
       # Shiny versions prior to 0.11 should use class = "modal" instead.
       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
@@ -45,36 +45,55 @@ navbarPage("Superzip", id="nav",
       tags$div(id="cite",
         'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
       )
+
     )
   ),
 
-  tabPanel("Data explorer",
-    fluidRow(
-      column(3,
-        selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
-      ),
-      column(3,
-        conditionalPanel("input.states",
-          selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
-        )
-      ),
-      column(3,
-        conditionalPanel("input.states",
-          selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
-        )
-      )
-    ),
-    fluidRow(
-      column(1,
-        numericInput("minScore", "Min score", min=0, max=100, value=0)
-      ),
-      column(1,
-        numericInput("maxScore", "Max score", min=0, max=100, value=100)
-      )
-    ),
+  tabPanel(
+           absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                         draggable = TRUE, top = "auto", left = "20", right = "auto", bottom = "20",
+                         width = "auto", height = "300",
+
+                         h2("ZIP explorer"),
+                         selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE),
+                         conditionalPanel("input.states",
+                                                selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+                                               ),
+                         conditionalPanel("input.states",
+                                                selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+                                               ),
+                        numericInput("minScore", "Min score", min=0, max=100, value=0)
+                                   ),
+                        numericInput("maxScore", "Max score", min=0, max=100, value=100)
+
+           ),
+    # fluidRow(
+    #   column(3,
+    #     selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+    #   ),
+    #   column(3,
+    #     conditionalPanel("input.states",
+    #       selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+    #     )
+    #   ),
+    #   column(3,
+    #     conditionalPanel("input.states",
+    #       selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+    #     )
+    #   )
+    # ),
+    # fluidRow(
+    #   column(1,
+    #     numericInput("minScore", "Min score", min=0, max=100, value=0)
+    #   ),
+    #   column(1,
+    #     numericInput("maxScore", "Max score", min=0, max=100, value=100)
+    #   )
+    # ),
     hr(),
     DT::dataTableOutput("ziptable")
-  ),
+  )
 
-  conditionalPanel("false", icon("crosshair"))
-)
+ # conditionalPanel("false", icon("crosshair"))
+
+
