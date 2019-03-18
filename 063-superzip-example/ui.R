81f6034f198fdd540a90499b13714cee319a8fac
Fengjunjie Pan <f.pan@campus.tu-berlin.de>
2019-03-18 21:08:21 +0100

    all in one page

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -1,4 +1,7 @@
+
+
 library(leaflet)
+library(ggvis)
 
 # Choices for drop-downs
 vars <- c(
@@ -9,91 +12,82 @@ vars <- c(
   "Population" = "adultpop"
 )
 
+fluidPage(
+  titlePanel("Movie explorer"),
 
-#navbarPage("Superzip", id="nav",
-tabsetPanel(
-  tabPanel(
-    div(class="outer",
+  fluidRow(
+    column(8,
+           div(class="innen",
 
-      tags$head(
-        # Include our custom CSS
-        includeCSS("styles.css"),
-        includeScript("gomap.js")
-      ),
+               tags$head(
+                 # Include our custom CSS
+                 includeCSS("styles.css"),
+                 includeScript("gomap.js")
+               ),
+               # If not using custom CSS, set height of leafletOutput to a number instead of percent
+               leafletOutput("map", width="100%", height="850")
+               # wellPanel(
+               #   span("Number of movies selected:",
+               #        textOutput("n_movies"),
+               #        h2("ZIP explorer"),
+               #        selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE),
+               #        conditionalPanel("input.states",
+               #                         selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+               #        ),
+               #        conditionalPanel("input.states",
+               #                         selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+               #        ),
+               #        numericInput("minScore", "Min score", min=0, max=100, value=0)
+               #
+               #
+               #
+               #
+               #   ),
 
-      # If not using custom CSS, set height of leafletOutput to a number instead of percent
-      leafletOutput("map", width="50%", height="50%"),
 
-      # Shiny versions prior to 0.11 should use class = "modal" instead.
-      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-        width = 330, height = "auto",
-
-        h2("ZIP explorer"),
-
-        selectInput("color", "Color", vars),
-        selectInput("size", "Size", vars, selected = "adultpop"),
-        conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
-          # Only prompt for threshold when coloring or sizing by superzip
-          numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
-        ),
-
-        plotOutput("histCentile", height = 200),
-        plotOutput("scatterCollegeIncome", height = 250)
-      ),
-
-      tags$div(id="cite",
-        'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
-      )
-
-    )
-  ),
-
-  tabPanel(
-           absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                         draggable = TRUE, top = "auto", left = "20", right = "auto", bottom = "20",
-                         width = "auto", height = "300",
-
-                         h2("ZIP explorer"),
-                         selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE),
-                         conditionalPanel("input.states",
-                                                selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
-                                               ),
-                         conditionalPanel("input.states",
-                                                selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
-                                               ),
-                        numericInput("minScore", "Min score", min=0, max=100, value=0)
-                                   ),
-                        numericInput("maxScore", "Max score", min=0, max=100, value=100)
-
+               )
            ),
-    # fluidRow(
-    #   column(3,
-    #     selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
-    #   ),
-    #   column(3,
-    #     conditionalPanel("input.states",
-    #       selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
-    #     )
-    #   ),
-    #   column(3,
-    #     conditionalPanel("input.states",
-    #       selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
-    #     )
-    #   )
-    # ),
-    # fluidRow(
-    #   column(1,
-    #     numericInput("minScore", "Min score", min=0, max=100, value=0)
-    #   ),
-    #   column(1,
-    #     numericInput("maxScore", "Max score", min=0, max=100, value=100)
-    #   )
-    # ),
-    hr(),
-    DT::dataTableOutput("ziptable")
+    column(4,
+           wellPanel(
+             h4("Filter"),
+             selectInput("color", "Color", vars),
+             selectInput("size", "Size", vars, selected = "adultpop"),
+             conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                              # Only prompt for threshold when coloring or sizing by superzip
+                              numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+
+
+             ),
+             wellPanel(
+               plotOutput("histCentile", height = 250)
+               #plotOutput("scatterCollegeIncome", height = 250)
+             ),
+
+             wellPanel(
+               selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE),
+               conditionalPanel("input.states",
+                                selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+               ),
+               conditionalPanel("input.states",
+                                selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+               ),
+               numericInput("minScore", "Min score", min=0, max=100, value=0),
+               numericInput("maxScore", "Max score", min=0, max=100, value=100)
+             )
+
+
+             )
+    )
+
+  ),
+  fluidRow(
+           hr(),
+           DT::dataTableOutput("ziptable")
+
   )
 
- # conditionalPanel("false", icon("crosshair"))
+  )
+
+
 
 
